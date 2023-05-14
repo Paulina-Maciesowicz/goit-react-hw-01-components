af907cacc63eeab5e9535e5793f9be3c49a9aec4
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-05-14 23:35:37 +0200

    u

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,3 +14,13 @@ export const App = () => {
     </div>
   );
 };
+
+import user from 'path/to/user.json;
+
+<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
