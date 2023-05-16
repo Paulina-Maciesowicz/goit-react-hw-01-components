6d859d893aa14d8899df4714e4992c1493b87c89
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-05-16 20:42:01 +0200

    u

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,27 +4,25 @@ import PropTypes from 'prop-types';
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <div className={css.profile}>
-      <div className={css.profile__container}>
-        <img className={css.profile__picture} src={avatar} alt="User avatar" />
-        <div className={css.information}>
-          <p className={css.information__username}> {username}</p>
-          <p className={css.information__tag}>@{tag}</p>
-          <p className={css.information__location}> {location}</p>
-        </div>
+      <img className={css.profile__picture} src={avatar} alt="User avatar" />
+      <div className={css.information}>
+        <p className={css.information__username}> {username}</p>
+        <p className={css.information__tag}>@{tag}</p>
+        <p className={css.information__location}> {location}</p>
       </div>
 
       <ul className={css.stats}>
-        <li>
-          <span className={css.stats__label}>followers {stats.followers}</span>
-          <span className={css.stats__quantity}></span>
+        <li className={css.stats__elem}>
+          <span className={css.quantity}>followers</span>
+          <span>{stats.followers}</span>
         </li>
-        <li>
-          <span className={css.stats__label}>views {stats.views}</span>
-          <span className={css.stats__quantity}></span>
+        <li className={css.stats__elem}>
+          <span className={css.quantity}>views</span>
+          <span>{stats.views}</span>
         </li>
-        <li>
-          <span className={css.stats__label}>likes {stats.likes}</span>
-          <span className={css.stats__quantity}></span>
+        <li className={css.stats__elem}>
+          <span className={css.quantity}>likes</span>
+          <span>{stats.likes}</span>
         </li>
       </ul>
     </div>
