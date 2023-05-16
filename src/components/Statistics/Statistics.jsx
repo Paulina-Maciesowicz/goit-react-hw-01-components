5cf6d3006b4387a595d16b75ebf7a29b06351cdd
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-05-16 22:26:42 +0200

    u

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import data from '../../data.json';
 const favouriteStatistics = data;
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
-{
-  getRandomHexColor();
-}
+// {
+//   getRandomHexColor();
+// }
 
 const StatisticsList = ({ statistics }) => {
   return (
