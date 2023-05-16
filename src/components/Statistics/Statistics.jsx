ca1bd339aaa4954bb1dc8ccd158ab1e482ccaf60
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-05-16 22:25:58 +0200

    u

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,21 +3,15 @@ import PropTypes from 'prop-types';
 import data from '../../data.json';
 const favouriteStatistics = data;
 
-const btn = document.querySelector('.change-color');
-const chageColor = document.querySelector('.color');
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-btn.addEventListener('click', anotherColor);
-function anotherColor() {
-  const color = getRandomHexColor();
-  chageColor.textContent = color;
-  document.body.style.backgroundColor = color;
+
+{
+  getRandomHexColor();
 }
-anotherColor();
 
 const StatisticsList = ({ statistics }) => {
   return (
