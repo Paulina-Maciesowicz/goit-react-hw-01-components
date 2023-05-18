65b27a04c3641eb585f118d183cbcf9be0146c9a
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-05-18 21:20:49 +0200

    u

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,13 @@
 import { StatisticsList } from './StatisticsList';
 import data from '../../data/data.json';
 import css from './Statistics.module.css';
-const favouriteStatistics = data;
+// const favouriteStatistics = data;
 
 export const Statistics = () => {
   return (
     <div className={css.statistics}>
       <h2 className={css.statistics__title}>Upload stats</h2>
-      <StatisticsList statistics={favouriteStatistics} />
+      <StatisticsList statistics={data} />
     </div>
   );
 };
